037954bf61bfd5e28637467d2b05a279f6e67937
Victor Roşca <49694231+roscavictor@users.noreply.github.com>
2021-05-12 03:39:08 +0300

    Add files via upload

---
+++ react/src/components/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+
+import {
+  Button,
+  FormGroup,
+  Input,
+  Container,
+  Row,
+  Col,
+  Form,
+} from "reactstrap";
+
+
+
+
+const Login = () => {
+  return (
+    <div className={styles.container}> 
+      <Container >
+        <Row className={styles.loginstuff}>
+        
+          <Col  className={styles.loginsquare} xs="4" >
+            <Form   >
+              <p className="text-center">Autentificare</p>
+              <FormGroup>
+                <div classname={styles.playerlist} >
+                  
+                  <Input 
+                    type="text"
+                    className="form-control py-4"
+                    placeholder="Username"
+                  />
+                </div>
+              </FormGroup>
+              <br/>
+              <div>
+                
+                <Input
+                  type="text"
+                  className="form-control py-4"
+                  placeholder="Password"
+                />
+              </div>
+              <br/>
+              <div>
+              <Link to="/MainPage">
+              <button type="button">
+                Login
+              </button>
+              </Link>
+              
+              </div>
+              <br/>
+              <button type="button">
+                Register
+              </button>
+              
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+      
+    </div>
+  );
+};
+
+export default Login;
---
+++ react/src/components/MainPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import styles from "./MainPage.module.css";
+import user from '../imgs/user.png'; // 
+import ticket from '../imgs/ticket.png';
+import ethereum from '../imgs/ethereum.png';
+var tickets=50;
+var cashmoney=3500;
+const MainPage = () => {
+ 
+  return (
+    <div className={styles.container}> 
+           <div className={styles.maintab}> 
+           <div className={styles.playerlist}> 
+            <p>Active players:</p>
+              <img src={user}/><p className={styles.playerlist}>Bot 1</p> <br/>
+              <img src={user}/><p className={styles.playerlist}>Bot 2</p><br/>
+              <img src={user}/><p className={styles.playerlist}>Bot 3</p><br/>
+              <img src={user}/><p className={styles.playerlist}>Bot 4</p><br/>
+              <img src={user}/><p className={styles.playerlist}>Bot 5</p><br/>
+            </div>
+            <div className={styles.pot}> 
+              <p style={{"marginLeft" : "150px"}}> Your pot:</p>
+              <p className={styles.totalpot}>  {tickets} </p> 
+              <img className={styles.ticketimg} src={ticket}/>
+              <p className={styles.money} >{cashmoney} <img className={styles.ticketimg} src={ethereum}/></p>
+              <button className={styles.cashoutbutton} >Buy in</button>
+              <br/>
+              <button className={styles.buyinbutton}>Cash out</button>
+            </div>
+              <div className={styles.betbuttons}>
+              <div className={styles.betbutton}>
+                <button> WIN 2X <img src={ticket}></img> </button>
+              </div>
+              <div className={styles.betbutton}>
+                <button style={{"backgroundColor":"green"}}>WIN 5X <img src={ticket}/></button></div>
+              </div>  
+            </div>
+    </div>
+  );
+};
+
+export default MainPage;
